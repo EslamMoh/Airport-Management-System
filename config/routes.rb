16cf470be5bcf9e84417363277f52a6222ff3261
Eslam Mohamed <eslammoh.ce@gmail.com>
2019-05-11 01:40:04 +0200

    change in flights controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     namespace :passenger do
-      resources :flights
+      resources :flights, only: [:show, :index]
     end
   end
 end
