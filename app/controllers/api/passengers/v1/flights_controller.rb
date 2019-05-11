4aab4f800bab7684f4b951b7ef383986edbcf422
Eslam Mohamed <eslammoh.ce@gmail.com>
2019-05-12 01:44:57 +0200

    edit in flights controller

--- app/controllers/api/passengers/v1/flights_controller.rb
+++ app/controllers/api/passengers/v1/flights_controller.rb
@@ -1,13 +1,15 @@
 module Api
   module Passengers
     module V1
-      class FlightsController < Api::Passengers::BaseController
+      class FlightsController < Api::BaseController
+        skip_before_action :authorize_request
         before_action :set_flight, only: [:show]
 
         def index
-          @flights = Flight.includes(:departure_airport, :destination_airport).all
-                          .page(page).per(per)
-          render json: PageDecorator.decorate(@flights).as_json(flight_details: true)
+          @flights = Flight.includes(:departure_airport, :destination_airport,
+                                     :flight_executions).all.page(page).per(per)
+          render json: PageDecorator.decorate(@flights)
+                                    .as_json(flight_details: true)
         end
 
         def show
