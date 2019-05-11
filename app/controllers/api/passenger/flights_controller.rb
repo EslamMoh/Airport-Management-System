fdd05d37f976683791dc2c3b62827082578425db
Eslam Mohamed <eslammoh.ce@gmail.com>
2019-05-11 06:51:45 +0200

    run rubocop for previous commit

--- app/controllers/api/passenger/flights_controller.rb
+++ app/controllers/api/passenger/flights_controller.rb
@@ -1,8 +1,9 @@
-class Api::Passenger::FlightsController < Api::BaseController
+class Api::Passenger::FlightsController < Api::Passenger::BaseController
   before_action :set_flight, only: [:show]
 
   def index
-    @flights = Flight.includes(:departure_airport, :destination_airport).all.page(page).per(per)
+    @flights = Flight.includes(:departure_airport, :destination_airport).all
+                     .page(page).per(per)
     render json: PageDecorator.decorate(@flights).as_json(flight_details: true)
   end
 
