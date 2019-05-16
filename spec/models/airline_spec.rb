45b705e69e2bb7e32784de21f7cc5863ec4da949
Eslam Mohamed <eslammoh.ce@gmail.com>
2019-05-17 00:24:09 +0200

    add airplane model spec file

--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Airline, type: :model do
   it { should have_many(:airplanes) }
   it { should have_many(:airport_airlines).dependent(:destroy) }
   it { should have_many(:airports).through(:airport_airlines) }
+
   # Validation test
   it { should validate_uniqueness_of(:name) }
 end
--- spec/models/airplane_spec.rb
+++ spec/models/airplane_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Airplane, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association test
+  it { should belong_to(:airline) }
+  it { should have_many(:flight_executions) }
+  it { should have_many(:seats) }
+
+  # Validation test
+  it { should validate_presence_of(:manufacturer) }
+  it { should validate_presence_of(:model_number) }
+  it { should validate_presence_of(:capacity) }
+  it { should validate_uniqueness_of(:model_number) }
 end
