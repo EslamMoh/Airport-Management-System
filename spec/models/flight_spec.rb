d9606eb6ac2961b34fc950d0063c1edffdc9e295
Eslam Mohamed <eslammoh.ce@gmail.com>
2019-05-17 18:47:00 +0200

    add flight spec

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Flight, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association test
+  it { should belong_to(:departure_airport).class_name('Airport') }
+  it { should belong_to(:destination_airport).class_name('Airport') }
+  it { should belong_to(:user) }
+  it { should have_many(:tickets) }
+  it { should have_many(:flight_flight_executions).dependent(:destroy) }
+  it { should have_many(:flight_executions).through(:flight_flight_executions) }
+
+  # Validation test
+  %i[departure_time arrival_time tickets_count destination_country 
+     departure_country status direction_type flight_type name price]
+    .each do |field|
+    it { should validate_presence_of(field) }
+  end
 end
