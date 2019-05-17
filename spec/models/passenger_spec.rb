dbe696d6642e86e4421539ed2bb7f2629e445d7c
Eslam Mohamed <eslammoh.ce@gmail.com>
2019-05-17 21:53:31 +0200

    add passenger spec

--- spec/models/passenger_spec.rb
+++ spec/models/passenger_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Passenger, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association test
+  it { should have_many(:tickets) }
+
+  # Validation test
+  %i[name email password_digest phone].each do |field|
+    it { should validate_presence_of(field) }
+  end
+  it { should validate_uniqueness_of(:email) }
 end
