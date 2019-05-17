5e4296a43485228aa05db66b391cfe52711c148a
Eslam Mohamed <eslammoh.ce@gmail.com>
2019-05-17 22:16:02 +0200

    add terminal spec

--- spec/models/terminal_spec.rb
+++ spec/models/terminal_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Terminal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association test
+  it { should belong_to(:airport) }
+  it do
+    should have_many(:destination_flight_executions)
+      .class_name('FlightExecution')
+      .with_foreign_key(
+        'destination_terminal_id'
+      )
+  end
+  it do
+    should have_many(:departure_flight_executions)
+      .class_name('FlightExecution')
+      .with_foreign_key(
+        'departure_terminal_id'
+      )
+  end
+
+  # Validation test
+  it { should validate_presence_of(:name) }
+  it { should validate_uniqueness_of(:name).scoped_to(:airport_id) }
 end
